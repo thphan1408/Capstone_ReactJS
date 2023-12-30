afe13f90b90ec2eef7a9837867d06eff4d69d01f
Huydevsg <huy666363@gmail.com>
2023-12-30 14:19:05 +0700

    revert commit 694eb7c6121b552b40a5539d516e7cd5e72903c6

--- src/layouts/AdminLayout/sections/list-movie/utils.js
+++ src/layouts/AdminLayout/sections/list-movie/utils.js
@@ -11,16 +11,15 @@ export const visuallyHidden = {
 }
 
 export function applyFilter({ inputData, comparator, filterName }) {
-  console.log('inputData: ', inputData)
-  // const stabilizedThis = inputData?.map((el, index) => [el, index])
+  const stabilizedThis = inputData?.map((el, index) => [el, index])
 
-  // stabilizedThis?.sort((a, b) => {
-  //   const order = comparator(a[0], b[0])
-  //   if (order !== 0) return order
-  //   return a[1] - b[1]
-  // })
+  stabilizedThis?.sort((a, b) => {
+    const order = comparator(a[0], b[0])
+    if (order !== 0) return order
+    return a[1] - b[1]
+  })
 
-  // inputData = stabilizedThis?.map((el) => el[0])
+  inputData = stabilizedThis?.map((el) => el[0])
 
   if (filterName) {
     inputData = inputData.filter(
@@ -29,7 +28,7 @@ export function applyFilter({ inputData, comparator, filterName }) {
     )
   }
 
-  // return inputData
+  return inputData
 }
 
 export function emptyRows(page, rowsPerPage, arrayLength) {
